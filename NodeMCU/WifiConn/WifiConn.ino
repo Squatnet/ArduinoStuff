8496b82bad7135bab1fba94588484c02d93827fa
Squat Net <squatn3t@gmail.com>
2018-09-21 20:32:51 +0100

    added basic nodeMCU wifi connect, webserver with buttons for 2 leds on / off.

---
+++ NodeMCU/WifiConn/WifiConn.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "SKY195AD";
+const char* password = "YYTRRFVSTQ";
+
+void setup() {
+  Serial.begin(9600);
+  // Initialize the output variables as outputs
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){}
