c00d6c9f16629becf0bfba66bcd3b81d3055b68a
Squat Net <squatn3t@gmail.com>
2018-10-17 19:13:31 +0100

    added 3matrix2strip version app. Needs testing but appears to create correct strings to send

---
+++ Godot HC05 App/3MAT2STRIPS/Settings.gd
@@ -0,0 +1,10 @@
+extends Panel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var option
+func _ready():
+	#$AddDevice/AddDevPnl/OptionButton/PopupMenu2.add_item("Strip")
+	#$AddDevice/AddDevPnl/OptionButton/PopupMenu2.add_item("Matrix")
+	pass
