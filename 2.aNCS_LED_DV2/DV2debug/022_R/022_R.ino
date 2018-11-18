3b0a34957491866881330d0414f544d71755ccc8
Riiio <Riolaurenti@googlemail.com>
2018-11-18 15:18:14 +0000

    try

--- 2.aNCS_LED_DV2/DV2debug/022_R/022_R.ino
+++ 2.aNCS_LED_DV2/DV2debug/022_R/022_R.ino
@@ -1,6 +1,15 @@
 // 022_R Router With PJON on 12 and 7, 3 analog pots and 1x 74hc595 LED array.
 
 // Aim : to Receive pulse signals and relay messages to "branch" of lights and relay board
+#include <PJONInteractiveRouter.h>
+
+StrategyLink<SoftwareBitBang> link1;
+StrategyLink<SoftwareBitBang> link2;
+
+PJONAny bus1(&link1, (const uint8_t[4]){0,0,1,53});
+PJONAny bus2(&link2, (const uint8_t[4]){0,0,0,2});
+
+PJONInteractiveRouter<PJONSwitch> router(2, (PJONAny*[2]) {&bus1, &bus2});
 
 #define LPIN 4 // Latch
 #define CPIN 5 // Clock
@@ -25,6 +34,12 @@ void hc595(int aa) {
         digitalWrite(LPIN, 1);
 }
 void setup() {
+  link1.strategy.set_pin(7);
+  link2.strategy.set_pin(12);
+  router.set_receiver(receiver_function);
+  router.set_router(true); // Pick up every packet passing
+  router.begin();
+  
   pinMode(LPIN, OUTPUT); // Init 595 (menu)
   dataArray[0] = 0b00000000;
   dataArray[1] = 0b11111100;
@@ -38,10 +53,39 @@ void setup() {
   dataArray[9] = 0b11111111;
   dataArray[10] = 0b10101010;
 }
-
+void receiver_handler(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  const char * arr = payload;
+  Serial.println(arr);
+  timeSinceBt = 0;
+  String str = arr;
+  Serial.println(str);
+  str.trim();
+  String strB = string.substring(0, 1);
+  str.remove(0, 1);
+  if (strB == "X") {
+    hc595(2);
+  }
+  else if (strB == "S") {
+    hc595(2);
+  }
+  /*
+  else {
+    x = strB.toInt(); // pattern choice 
+    if (str.length() == 6) {
+      char charbuf[8];
+      str.toCharArray(charbuf, 8);
+      long int rgb = strtol(charbuf, 0, 16); //=>rgb=0x001234FE;
+      byte r = (byte)(rgb >> 16);
+      byte g = (byte)(rgb >> 8);
+      byte b = (byte)(rgb);
+      ourCol = CRGB(r, g, b); // set the colour value!
+    }
+  }
+  */
+  str = "";
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ router.loop();
 }
 
 void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
