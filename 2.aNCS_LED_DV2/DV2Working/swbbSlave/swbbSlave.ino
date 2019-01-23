3ef34714f71a8cbcb1fcb3638ab1f414740cadd0
Squat nET <squatn3t@gmail.com>
2019-01-23 17:04:33 +0000

    Standarised slave files, Fully commented TFT sketch re #34

--- 2.aNCS_LED_DV2/DV2Working/swbbSlave/swbbSlave.ino
+++ 2.aNCS_LED_DV2/DV2Working/swbbSlave/swbbSlave.ino
@@ -1,4 +1,5 @@
 // PJON stuff //
+#define PJON_INCLUDE_SWBB
 #include <PJONSlave.h>  // we are inslave mode .
 uint8_t bus_id[] = {0, 0, 1, 53}; // Ancs unique bus ID
 PJONSlave<SoftwareBitBang> bus(bus_id, PJON_NOT_ASSIGNED); // Force no id so master can assign us
@@ -12,6 +13,44 @@ int ourID = 255; // Our ID number
 String string = ""; // holder for our Pjon Message
 bool ack = false; // Master has registered us?
 void(* resetFunc) (void) = 0; // Software reset hack
+
+// PJON error handling 
+void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
+  if(code == PJON_CONNECTION_LOST) { // Master is gone !!
+    Serial.print("Connection lost with device ");
+    Serial.println((uint8_t)bus.packets[data].content[0], DEC);
+    delay(1000); // wait a second
+    resetFunc(); // Reset
+  }
+  if(code == PJON_ID_ACQUISITION_FAIL) { // Didnt get an addres... !
+    if(data == PJON_ID_ACQUIRE) {
+      Serial.println("PJONSlave error: multi-master addressing failed.");
+      // Didnt get id in Multi-Master environment
+      delay(1000);
+      resetFunc(); // bye!
+    }
+    if(data == PJON_ID_CONFIRM) // failed to confirm id with master... This shouldnt happen
+      Serial.println("PJONSlave error: master-slave id confirmation failed.");
+    if(data == PJON_ID_NEGATE)
+      // We wont encounter this as we dont intend to give up out ID
+      Serial.println("PJONSlave error: master-slave id release failed.");
+    if(data == PJON_ID_REQUEST)
+      // We couldnt find a Master on the network.... 
+      Serial.println("PJONSlave error: master-slave id request failed.");
+      delay(400); // wait 400ms
+      if (millis() > 15000){ // if 15s has passed
+        Serial.println("Resetting due to no ID");
+        delay(300); // we reset
+        resetFunc();
+        } // if not
+      bus.acquire_id_master_slave(); // try and get an id again
+      acquired = false; // make sure we resend our registration when we reconnect
+      delay(160); // makes the delay about 500ms between retrys
+     
+ }
+  Serial.flush(); // wait til serial is printed
+};
+
 // PJON RECEIVER CODE
 void receiver_handler(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
    const char * arr = payload; // Not a pointer now.... !
@@ -53,52 +92,22 @@ void parser(){
   Serial.print("STR = "); // prints after length < 1
   Serial.println(string);
   string = ""; // empty it
-}
-// PJON error handling 
-void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
-  if(code == PJON_CONNECTION_LOST) { // Master is gone !!
-    Serial.print("Connection lost with device ");
-    Serial.println((uint8_t)bus.packets[data].content[0], DEC);
-    delay(1000); // wait a second
-    resetFunc(); // Reset
-  }
-  if(code == PJON_ID_ACQUISITION_FAIL) { // Didnt get an addres... !
-    if(data == PJON_ID_ACQUIRE) {
-      Serial.println("PJONSlave error: multi-master addressing failed.");
-      // Didnt get id in Multi-Master environment
-      delay(1000);
-      resetFunc(); // bye!
-    }
-    if(data == PJON_ID_CONFIRM) // failed to confirm id with master... This shouldnt happen
-      Serial.println("PJONSlave error: master-slave id confirmation failed.");
-    if(data == PJON_ID_NEGATE)
-      // We wont encounter this as we dont intend to give up out ID
-      Serial.println("PJONSlave error: master-slave id release failed.");
-    if(data == PJON_ID_REQUEST)
-      // We couldnt find a Master on the network.... 
-      Serial.println("PJONSlave error: master-slave id request failed.");
-      delay(400); // wait 400ms
-      if (millis() > 15000){ // if 15s has passed
-        Serial.println("Resetting due to no ID");
-        delay(300); // we reset
-        resetFunc();
-        } // if not
-      bus.acquire_id_master_slave(); // try and get an id again
-      acquired = false; // make sure we resend our registration when we reconnect
-      delay(160); // makes the delay about 500ms between retrys
-     
- }
-  Serial.flush(); // wait til serial is printed
 };
-
 void setup() {  // SETUP 
   Serial.begin(115200); // Serial console
+  Serial.print("Setup ");
   bus.set_error(error_handler); // link PJON to error handler
+  Serial.print(". ");
   bus.set_receiver(receiver_handler); // link PJON to receiver
+  Serial.print(". ");
   bus.strategy.set_pin(12); // Set PJON pin
+  Serial.print(". ");
   bus.begin(); // 
+  Serial.print(". ");
   delay(160); // possibly not needed if master is online
+  Serial.print(". ");
   bus.acquire_id_master_slave(); //get an id
+  Serial.print(". ");
   Serial.println("Done!!!");
   // SETUP FINISHES
 }
--- 2.aNCS_LED_DV2/DV2Working/tftBPJONv1/tftBPJONv1.ino
+++ 2.aNCS_LED_DV2/DV2Working/tftBPJONv1/tftBPJONv1.ino
@@ -1,23 +1,28 @@
-#include <PJONSlave.h>
+// PJON stuff //
+#define PJON_INCLUDE_SWBB
+#include <PJONSlave.h>  // we are inslave mode .
+uint8_t bus_id[] = {0, 0, 1, 53}; // Ancs unique bus ID
+PJONSlave<SoftwareBitBang> bus(bus_id, PJON_NOT_ASSIGNED); // Force no id so master can assign us
+int packet; // Holder for our packet
+bool acquired = false; // did we get an address? 
+bool debugMode = false; // Are we debugging
+uint32_t t_millis; // tick tock
+int ourID = 255; // Our ID number 
 
-// PJON object
+// TFT Stuff //
 #include <Adafruit_GFX.h>
 #include <Adafruit_SPITFT.h>
 #include <Adafruit_SPITFT_Macros.h>
 #include <gfxfont.h>
 #include "Adafruit_GFX.h"// Hardware-specific library
 #include <MCUFRIEND_kbv.h> // TFT Stuff
-
-uint8_t bus_id[4] = {0,0,1,53}; // aNCS Unique Bus ID :)
-
-PJONSlave<SoftwareBitBang> bus(bus_id, PJON_NOT_ASSIGNED);
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
-MCUFRIEND_kbv tft; // assign tft mcufriend related stuff to an alias - tft
-/**************Declare human readable colors *****************/
+MCUFRIEND_kbv tft; // Creates the tft object
+// Declare human readable colors for TFT //
 #define  BLACK   0x0000
 #define BLUE    0x001F
 #define RED     0xF800
@@ -26,117 +31,115 @@ MCUFRIEND_kbv tft; // assign tft mcufriend related stuff to an alias - tft
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
-#ifndef min
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-#endif
-void(* resetFunc) (void) = 0;
-void progmemPrint(const char *str);
-void progmemPrintln(const char *str);
-int ourID = 255;
-int packet;
-char content[] = "01234567890123456789";
-bool acquired = false;
 
-void receiver_handler(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
-  Serial.print("Received: ");
-  for(uint16_t i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
-  Serial.flush();
-};
+// define min if not already //
+#ifndef min // if not already defined
+#define min(a, b) (((a) < (b)) ? (a) : (b)) // this is how you work out min
+#endif 
+
+// Program vars //
+void progmemPrint(const char *str); // Dont actually know what these 2 are for? 
+void progmemPrintln(const char *str); // Rio shed any light? 
+String string = ""; // hold incoming PJON message
+bool ack = false; // Master has registered us?
+void(* resetFunc) (void) = 0; // Software reset 
 
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
-  if(code == PJON_CONNECTION_LOST) {
+  if(code == PJON_CONNECTION_LOST) { // Master is gone !!
     Serial.print("Connection lost with device ");
     Serial.println((uint8_t)bus.packets[data].content[0], DEC);
-    delay(1000);
-    resetFunc();
+    delay(1000); // wait a second
+    resetFunc(); // Reset
   }
-  if(code == PJON_ID_ACQUISITION_FAIL) {
+  if(code == PJON_ID_ACQUISITION_FAIL) { // Didnt get an addres... !
     if(data == PJON_ID_ACQUIRE) {
       Serial.println("PJONSlave error: multi-master addressing failed.");
+      // Didnt get id in Multi-Master environment
       delay(1000);
-      resetFunc();
+      resetFunc(); // bye!
     }
-    if(data == PJON_ID_CONFIRM)
+    if(data == PJON_ID_CONFIRM) // failed to confirm id with master... This shouldnt happen
       Serial.println("PJONSlave error: master-slave id confirmation failed.");
     if(data == PJON_ID_NEGATE)
+      // We wont encounter this as we dont intend to give up out ID
       Serial.println("PJONSlave error: master-slave id release failed.");
     if(data == PJON_ID_REQUEST)
+      // We couldnt find a Master on the network.... 
       Serial.println("PJONSlave error: master-slave id request failed.");
-      delay(400);
-      if (millis() > 15000){
+      delay(400); // wait 400ms
+      if (millis() > 15000){ // if 15s has passed
         Serial.println("Resetting due to no ID");
-        delay(300);
+        delay(300); // we reset
         resetFunc();
-        }
-      bus.acquire_id_master_slave();
-      acquired = false;
-      delay(160);
+        } // if not
+      bus.acquire_id_master_slave(); // try and get an id again
+      acquired = false; // make sure we resend our registration when we reconnect
+      delay(160); // makes the delay about 500ms between retrys
      
  }
-  Serial.flush();
+  Serial.flush(); // wait til serial is printed
+};
+// PJON Receiver function //
+void receiver_handler(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  uint8_t id = packet_info.sender_id;
+  const char * arr = payload; // Not a pointer now.... !
+  string.concat(arr); // addd it to our string
+  if( string.startsWith("ack")){ // master got our registation message!
+    ack = true; // nice
+    string = ""; // thats all it says!
+    Serial.print("Heard from server");
+    }
+  if (string.startsWith("Rst")){ // Is it a reset message?
+    resetFunc(); // Device resets
+    }
+  else{ 
+    Serial.print(string); // Print the message to serial
+    string = ""; // Empty the message
+    mScreen(arr,length,id); // send arr to the TFT as its already char array (nice!)
+    }
 };
 
 void setup() {
-  Serial.begin(115200);
-  bus.set_error(error_handler);
-  Serial.println("Setup");
-  delay(1060);
-  bus.strategy.set_pin(12);
-  bus.set_receiver(receiver_function);
-  bus.acquire_id_master_slave();
-  bus.begin();
-  delay(160);
-  Serial.print("Bus init ");
-  Serial.println(bus.device_id());
-  //bus.send(1, "B", 1);
- uint16_t ID = tft.readID();
-    tft.begin(ID);
-    menuScreen();
-    
-}
-void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
-  /* Make use of the payload before sending something, the buffer where payload points to is
-     overwritten when a new message is dispatched */
-     if(payload[0] != '\0'){
-      uint8_t id = packet_info.sender_id;
-      String st = "";
-      const char * arr = payload;
-      st.concat(arr);
-      if (st.startsWith("ack")){tft.println("Registered Successfully With MASTER");}
-      if (st.startsWith("Rst")){resetFunc();}
-      else{ mScreen(arr,length,id);
-     for(uint8_t i = 0; i < length; i++) {
-      Serial.print(char( payload[i]));
-     }
-    Serial.println(" ");
-    }
-  }
+  Serial.begin(115200); // Serial console
+  Serial.print("Setup ");
+  bus.set_error(error_handler); // link PJON to error handler
+  Serial.print(". ");
+  bus.set_receiver(receiver_handler); // link PJON to receiver
+  Serial.print(". ");
+  bus.strategy.set_pin(12); // Set PJON pin
+  Serial.print(". ");
+  bus.begin(); // 
+  Serial.print(". ");
+  delay(160); // possibly not needed if master is online
+  Serial.print(". ");
+  bus.acquire_id_master_slave(); //get an id
+  Serial.print(". ");
+  Serial.println("Done!!!");
+  uint16_t ID = tft.readID(); // read the TFT id 
+  tft.begin(ID); // Begin connection with TFT
+  menuScreen(); // Draw the initial Screen    
 }
 void loop() {
-  if(ourID == 255 && millis() > 15000){
+  if(ourID == 255 && millis() > 15000){ // No id has been assigned and 15s have elapsed
     Serial.println("NO ID AFTER 15000");
-    resetFunc();
+    resetFunc(); // reset
   }
-  if((bus.device_id() != PJON_NOT_ASSIGNED) && !acquired) {
+  if((bus.device_id() != PJON_NOT_ASSIGNED) && !acquired) { // we have an id, but havent regisrtered
     Serial.print("Acquired device id: ");
-    Serial.println(bus.device_id());
-    ourID = bus.device_id();
-    Serial.flush();
+    Serial.println(bus.device_id()); 
+    ourID = bus.device_id(); // Set that shit
+    Serial.flush(); // finish writing serial
     delay(100);
-    doFooter();
-    acquired = true;
-    tellMasterAboutSelf();
+    doFooter(); // Redraw the footer to update the ID
+    acquired = true; // track that
+    tellMasterAboutSelf(); // and register
   }
-  bus.update();
-  bus.receive(5000);
-}
+  bus.update(); // update the PJON
+  bus.receive(5000); // receive for a while
+} //END
+// prints a line to the TFT // 
 unsigned long mScreen(uint8_t *payload,uint16_t length,int sender) { // make this receive char* or string whatever :) have fun
     unsigned long start;
-  
     tft.setTextColor(WHITE,BLACK);
     tft.setTextSize(1);
     String newline = "";
@@ -147,6 +150,7 @@ unsigned long mScreen(uint8_t *payload,uint16_t length,int sender) { // make thi
     tft.println(sender);
   return micros() - start;
 }
+// Draws the header //
 void doHeader(){
     tft.setTextColor(WHITE);
     tft.setTextSize(2); tft.println(" autoNoiseCreationStation");
@@ -155,6 +159,7 @@ void doHeader(){
     tft.setTextColor(RED);
     tft.setTextSize(1); tft.println("Interactive Omnipresent Lighting Network ");
 }
+// Draws the footer //
 void doFooter(){
       //FOOTER
     tft.setCursor(0, 464);
@@ -169,12 +174,14 @@ void doFooter(){
     tft.println("       V0.1");
      tft.setCursor(0,50);
 }
+// Registers with the master // 
 void tellMasterAboutSelf(){
   String newPkt = "Reg,Term,MasterTerm";
   const char pkt[newPkt.length()+1];
   newPkt.toCharArray(pkt,newPkt.length());
   bus.send_packet(254,pkt,newPkt.length()+1);
 }
+// Draws the full screen //
 unsigned long menuScreen() {
     unsigned long start;
     //SETUP
