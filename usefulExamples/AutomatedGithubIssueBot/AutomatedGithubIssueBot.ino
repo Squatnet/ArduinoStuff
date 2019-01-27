d965d794eb1dde37d52f26e9948f1747a699c41b
Squat nET <squatn3t@gmail.com>
2019-01-27 16:39:18 +0000

    Automated github issue creation, see #39 for details

---
+++ usefulExamples/AutomatedGithubIssueBot/AutomatedGithubIssueBot.ino
@@ -0,0 +1,13 @@
+/**
+ * @todo Automated issue creation
+ * @body These lines of comment will automatically create an issue when being commited"
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
