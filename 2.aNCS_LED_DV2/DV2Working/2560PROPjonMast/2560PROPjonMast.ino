d3a8b5f79f345c4e9b1a2b8d7979d8c9b14080aa
Squat nET <squatn3t@gmail.com>
2019-01-28 20:28:52 +0000

    ahem #34

--- 2.aNCS_LED_DV2/DV2Working/2560PROPjonMast/2560PROPjonMast.ino
+++ 2.aNCS_LED_DV2/DV2Working/2560PROPjonMast/2560PROPjonMast.ino
@@ -1,4 +1,4 @@
-/*
+/**
  * @todo Make DV2 Pjon master receive i2c clock pulses
  * @body Master should broadcast BPM if it ever changes and broadcast a pulse on 4, 16, 32, 64 clock messages received. 
  */
