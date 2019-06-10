15de831285d708543605d0b3c776a40e8944c191
kieran-boyle <kieran.boyle1@outlook.com>
2019-06-10 19:54:04 +0100

    Fix #100. Will comment in detail inline, simple stuff. new issue #104.

--- 2.aNCS_LED_DV2/current/I2CtoFastLEDs/I2CtoFastLEDs.ino
+++ 2.aNCS_LED_DV2/current/I2CtoFastLEDs/I2CtoFastLEDs.ino
@@ -39,7 +39,7 @@ int y = 0;//holder for i2c message which sets pattern when we adressing strips i
 int timeSinceBt = 0; //legacy currently unused. set to 0 when message comes in, then increments each second (time since last message recieved.)??
 int autoMode = 1;//if 1 increments the pattern and palette. if 2 only increments palette.
 int autoSecs = 4;//sets the upper bound for timeSinceBt function.
-int stripNumber = 1;//stores the strip that we wish to set the pattern on. 
+int stripNumber = 1;//stores the strip that we wish to set the pattern on.
 int individualStripMode = 0;//holds wether we are addressing all the stips(0)or individual strips (1)
 int paletteMode = 1;//holds if we sending indivdual colors to the patterns or a palette array.
 int paletteNumber = 0;//holds the number for which palette is in use when paletteMode is on.
@@ -613,7 +613,7 @@ void parser() {
 
 void randPattern() {//choses a random pattern
   if (individualStripMode == 0) {
-    patternNumber = random(2, 10);//increase by one when strobe is fixed
+    patternNumber = random(2, 11);//increase by one when strobe is fixed
     DPRINT("RANDOM ");
     DPRINTLN(patternNumber);
   }
@@ -622,7 +622,7 @@ void randPattern() {//choses a random pattern
     int lastStripNumber = stripNumber;
     FL(1, NUM_STRIPS + 1) {
       if (i == stripNumber) {
-        patternStore[i] = random(2, 10);//increase by one when strobe is fixed
+        patternStore[i] = random(2, 11);//increase by one when strobe is fixed
         DPRINTLN(patternStore[i]);
       }
     }
@@ -897,7 +897,82 @@ void juggle() {//palette not currently supported
   }
   currentDelay = myDelay;
 }
-
+void simpleStrobe () {
+  int myDelay = map(variableDelay, 0, 255, (standardDelay / 8), (standardDelay * 1.5));
+  turnOff();
+  const uint8_t kStrobeCycleLength = 6; // light every Nth frame
+  static uint8_t sStrobePhase = 0;
+  sStrobePhase = sStrobePhase + 1;
+  colorIndex++;
+  if (colorIndex > 256) {
+    colorIndex = 1;
+  }
+  if ( sStrobePhase >= kStrobeCycleLength ) {
+    sStrobePhase = 0;
+  }
+  if ( sStrobePhase == 0 ) {
+    uint8_t dashperiod = beatsin8( 8/*cycles per minute*/, 4, 10);
+    uint8_t dashwidth = (dashperiod / 4) + 1;
+    uint8_t zoomBPM = STROBE_BEATS_PER_MINUTE;
+    int8_t  dashmotionspeed = beatsin8( (zoomBPM / 2), 1, dashperiod);
+    if (dashmotionspeed >= (dashperiod / 2)) {
+      dashmotionspeed = 0 - (dashperiod - dashmotionspeed );
+    }
+    uint8_t cycle = beat8(2); // two cycles per minute
+    uint8_t easedcycle = ease8InOutCubic( ease8InOutCubic( cycle));
+    uint8_t wavecycle = cubicwave8( easedcycle);
+    // uint8_t hueShift = 0; // NO SHIFT OF HUE IN COLOUR (we should rebuild in RGB...)
+    uint8_t hueShift = scale8( wavecycle, 130); // METHOD HOW HUE VALUE SHIFTS
+    uint8_t strobesPerPosition = 2; // try 1..4
+    strobeCore( dashperiod, dashwidth, dashmotionspeed, strobesPerPosition, hueShift);
+  }
+  if (individualStripMode == 1) {
+    delayStore[stripNumber] = myDelay;
+  }
+  currentDelay = myDelay;
+}
+void strobeCore(uint8_t dashperiod, uint8_t dashwidth, int8_t  dashmotionspeed, uint8_t stroberepeats, uint8_t huedelta) {
+  static uint8_t sRepeatCounter = 0;
+  static int8_t sStartPosition = LEDStart;
+  static uint8_t sStartHue = 0;
+  sStartHue += 1; //Shift the Colour little by little
+  sRepeatCounter = sRepeatCounter + 1;
+  if ( sRepeatCounter >= stroberepeats) {
+    sRepeatCounter = 0;
+    sStartPosition = sStartPosition + dashmotionspeed;
+    if ( sStartPosition >= dashperiod ) {
+      while ( sStartPosition >= dashperiod) {
+        sStartPosition -= dashperiod;
+      }
+      sStartHue  -= huedelta;
+    }
+    else if ( sStartPosition < 0) {
+      while ( sStartPosition < 0) {
+        sStartPosition += dashperiod;
+      }
+      sStartHue  += huedelta;
+    }
+  }
+  const uint8_t kSaturation = 208; // WHITE >> CURRENT COLOUR control (def 208)
+  const uint8_t kValue = 200; // Brightness??
+  strobeDraw( sStartPosition, LEDEnd - 1, dashperiod, dashwidth, sStartHue, huedelta, kSaturation, kValue);
+}
+static void strobeDraw(uint8_t startpos, uint16_t lastpos, uint8_t period, uint8_t width, uint8_t huestart, uint8_t huedelta, uint8_t saturation, uint8_t value) {
+  uint8_t hue = huestart;
+  for ( uint16_t i = startpos; i <= lastpos; i += period) {
+    CRGB color = CHSV( hue, saturation, value);
+    //CRGB color = CRGB::Blue; // USE TO COMPLETELY BYPASS HSV Change Scheme
+    uint16_t pos = i;
+    for ( uint8_t w = 0; w < width; w++) {
+      leds[pos] = ColorFromPalette(currentPalette, colorIndex);
+      pos++;
+      if (pos >= LEDEnd) {
+        break;
+      }
+    }
+    hue += huedelta;
+  }
+}
 void paletteSelect() {//selects palette
   switch (paletteNumber) {
     case 0:
@@ -995,7 +1070,9 @@ void patternSelect() {//selects pattern
       case 9:
         juggle();
         break;
-      
+      case 10:
+        simpleStrobe();
+        break;
     }
   }
   if (individualStripMode == 1) { //second switch case used to set patternSelect array.
@@ -1033,6 +1110,9 @@ void patternSelect() {//selects pattern
           break;
         case 9:
           juggle();
+          break;
+        case 10:
+          simpleStrobe();
           break;
       }
     }
@@ -1111,7 +1191,7 @@ void loop() {
   patternSelect();
   //FastLED.show();
   if (individualStripMode == 1) {
-    int sum = delayStore[1]+delayStore[2]+delayStore[3]+delayStore[4];
+    int sum = delayStore[1] + delayStore[2] + delayStore[3] + delayStore[4];
     float avg = sum * 0.25;
     FastLED.delay((int)avg);
   }
